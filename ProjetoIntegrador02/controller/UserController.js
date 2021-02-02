623d7ad224795b702902211d03ce85bce3e48915
Jackson Mateus <jack.matheusch@gmail.com>
2021-02-02 16:52:16 -0300

    Evolução da pagina do estabelecimento

---
+++ ProjetoIntegrador02/controller/UserController.js
@@ -0,0 +1,10 @@
+const users = require('../data/user');
+
+module.exports = {
+    // create(req, res, next){
+    //   res.render('create-user');
+    // },
+
+    // estabelecimento (req, res, next){
+    // res.redirect('/');
+  }
--- ProjetoIntegrador02/routes/users.js
+++ ProjetoIntegrador02/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const UserController=require('../controller/UserController');
+
+/* GET users listing. */ 
+
+
+// router.get('/estabeleciemento', UserController.estabelecimento)
 
 module.exports = router;
