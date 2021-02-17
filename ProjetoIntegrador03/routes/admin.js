025b1ae29b4efe38ec3b4b25c214bd6d73ea266e
Jackson Mateus <jack.matheusch@gmail.com>
2021-02-17 13:39:20 -0300

    Atualização das Imagens pagina do restaurante

--- ProjetoIntegrador03/routes/admin.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const AdminControllers = require('../controllers/AdminControllers');
-var router = express.Router();
-const RestaurantController = require('../controllers/AdminControllers');
-
-router.get('/', AdminControllers.index )
-
-
-module.exports = router;
