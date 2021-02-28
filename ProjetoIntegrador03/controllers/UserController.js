6f343de7faf2a76b7af9d01dfcfa8dd7680ce681
joao1ima <j.airon@hotmail.com>
2021-02-27 22:12:30 -0300

    criado formulário de cadastro com configuração de salvamento de dados em json

--- ProjetoIntegrador03/controllers/UserController.js
+++ ProjetoIntegrador03/controllers/UserController.js
@@ -1,11 +1,47 @@
-// const bcrypt = require('bcrypt');
-// const users = require('../data/user');
-// const saveData = require('../utils/saveData');
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcrypt');
+const path = require('path');
+const {check, validationResult, body} = require('express-validator');
+const fs = require('fs');
+const allUsers = path.join('data', 'users.json')
 
 module.exports = {
   create(req, res, next){
     res.render('create-user');
   },
+
+  salve(req, res, next){
+    let {
+      nome,
+      celular,
+      email,
+      cep,
+      logradouro,
+      complemento,
+      cidade,
+      estado,
+      senha
+    } = req.body
+
+    
+    let senhaCripto = bcrypt.hashSync(senha, 12);
+    let dadosJson = JSON.stringify({
+      nome,
+      celular,
+      email,
+      cep,
+      logradouro,
+      complemento,
+      cidade,
+      estado,
+      senha:senhaCripto
+    })
+    fs.appendFileSync(allUsers, dadosJson)
+    res.send(dadosJson);
+  },
+
+  
  
   login(req, res, next){
     res.render('login');
