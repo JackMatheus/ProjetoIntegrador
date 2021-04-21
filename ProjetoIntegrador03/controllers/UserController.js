3d625fdf0f9ba0899843547dd1971aca6d4b54a6
joao1ima <j.airon@hotmail.com>
2021-04-21 17:56:54 -0300

    atualizando setup de sessão

--- ProjetoIntegrador03/controllers/UserController.js
+++ ProjetoIntegrador03/controllers/UserController.js
@@ -34,15 +34,14 @@ module.exports = {
   async authenticate(req, res, next){
     let { email, password } = req.body;
     let user = await Usuario.findOne({ where: { email } });
-    // console.log(user)
-    // return
-    // if(!user){
-    // return res.render('login/authenticate', { notFound: true });
-    // }
+    
+    if(!user){
+    return res.render('login', { notFound: true });
+    }
 
-    // if(!bcrypt.compareSync(password, user.password)){
-    // return res.render('login/authenticate', { notFound: true });
-    // }
+    if(!bcrypt.compareSync(password, user.password)){
+    return res.render('login', { notFound: true });
+    }
     
     // removendo o valor propriedade password para que o usuario logado nao trafegue com sua senha
     user.password = undefined;
