6ddb4e5769ad4f798dd0c9e402f1e0bdd688c43c
joao1ima <j.airon@hotmail.com>
2021-05-01 19:27:49 -0300

    correcao middleware

--- ProjetoIntegrador03/middleware/auth.js
+++ ProjetoIntegrador03/middleware/auth.js
@@ -1,9 +1,9 @@
 function auth (req, res, next){
-    console.log(req.session.user)
+    
     if (typeof(req.session.user) != 'undefined'){
         return next();
     } else {
-        return res.send('Para realizar uma rezerva você precisa fazer login no sitema!')
+        return res.send('Para realizar uma rezerva você precisa fazer login no sistema!')
     }
 }
 
